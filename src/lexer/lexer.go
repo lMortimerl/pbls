52f0c068bfc4a142175271b0d90a145b38459982
Moritmer Gibbons <mkirchermeier@gmail.com>
2024-11-17 16:35:39 +0100

    Add: String/Identifier parsing Added parsing of strings, identifiers and comments.

--- src/lexer/lexer.go
+++ src/lexer/lexer.go
@@ -21,12 +21,99 @@ type Lexer struct {
 	column   int
 }
 
+func (l *Lexer) advanceN(n int) {
+	l.current += n
+	l.column += n
+	if l.current < len(l.source) {
+		curRune := rune(l.source[l.current])
+		if curRune == '\n' {
+			l.line++
+			l.column = 1
+		}
+	}
+}
+func (l *Lexer) push(t Token) {
+	l.Tokens = append(l.Tokens, t)
+}
+func (l *Lexer) at() string {
+	return string(l.source[l.current])
+}
+func (l *Lexer) remainder() string {
+	return l.source[l.current:]
+}
+func (l *Lexer) atEOF() bool {
+	return l.current >= len(l.source)
+}
+func Tokenize(source []byte) []Token {
+	lex := NewLexer(source)
+
+	for !lex.atEOF() {
+		matched := false
+
+		for _, pattern := range lex.patterns {
+			loc := pattern.regex.FindStringIndex(string(lex.remainder()))
+
+			if loc != nil && loc[0] == 0 {
+				pattern.handler(lex, pattern.regex)
+				matched = true
+				break
+			}
+		}
+
+		if !matched {
+			panic(fmt.Sprintf("Lexer::Error -> unrecoginized token at line %d column %d near %s\n", lex.line, lex.column, string(lex.remainder())))
+		}
+	}
+
+	lex.push(NewToken(EOF, "EOF", lex.line, lex.column))
+
+	return lex.Tokens
+}
+
+func numberHandler(lex *Lexer, regex *regexp.Regexp) {
+	match := regex.FindString(string(lex.remainder()))
+	lex.push(NewToken(NUMBER, match, lex.line, lex.column))
+	lex.advanceN(len(match))
+}
+func skipHandler(lex *Lexer, regex *regexp.Regexp) {
+	match := regex.FindStringIndex(string(lex.remainder()))
+	lex.advanceN(match[1])
+}
+func stringHandler(lex *Lexer, regex *regexp.Regexp) {
+	match := regex.FindStringIndex(lex.remainder())
+	stringLiteral := lex.remainder()[match[0]+1:match[1]-1]
+
+	lex.push(NewToken(STRING, stringLiteral, lex.line, lex.column))
+	lex.advanceN(len(stringLiteral)+2)
+}
+func symbolHandler(lex *Lexer, regex *regexp.Regexp) {
+	match := regex.FindString(lex.remainder())
+
+	if kind, exists := reserved_lu[match]; exists {
+		lex.push(NewToken(kind, match, lex.line, lex.column))
+	} else {
+		lex.push(NewToken(IDENTIFIER, match, lex.line, lex.column))
+	}
+
+	lex.advanceN(len(match))
+}
+func defaultHandler(kind TokenKind, value string) regexHandler {
+	return func(lex *Lexer, regex *regexp.Regexp) {
+		lex.advanceN(len(value))
+		lex.push(NewToken(kind, value, lex.line, lex.column))
+	}
+}
 func NewLexer(source []byte) *Lexer {
 	return &Lexer{
 		source: string(source),
 		line:   1,
 		patterns: []regexPattern{
+			{regexp.MustCompile(`[a-zA-Z_][a-zA-Z_0-9]*`), symbolHandler},
 			{regexp.MustCompile(`[0-9]+(\.[0-9]+)?`), numberHandler},
+			{regexp.MustCompile(`"[^"]*"`), stringHandler},
+			{regexp.MustCompile(`'[^"]*'`), stringHandler},
+			{regexp.MustCompile(`\/\/.*`), skipHandler},
+			{regexp.MustCompile(`\/\*[^*\/]*\/`), skipHandler},
 			{regexp.MustCompile(`\s+`), skipHandler},
 
 			{regexp.MustCompile(`\[`), defaultHandler(OPEN_BRACKET, "[")},
@@ -167,69 +254,5 @@ func NewLexer(source []byte) *Lexer {
 				{regexp.MustCompile(`_debug`), defaultHandler(_DEBUG, "_debug")},
 			*/
 		},
-	}
-}
-func (l *Lexer) advanceN(n int) {
-	l.current += n
-	l.column += n
-	if l.current < len(l.source) {
-		curRune := rune(l.source[l.current])
-		if curRune == '\n' {
-			l.line++
-			l.column = 1
-		}
-	}
-}
-func (l *Lexer) push(t Token) {
-	l.Tokens = append(l.Tokens, t)
-}
-func (l *Lexer) at() string {
-	return string(l.source[l.current])
-}
-func (l *Lexer) remainder() string {
-	return l.source[l.current:]
-}
-func (l *Lexer) atEOF() bool {
-	return l.current >= len(l.source)
-}
-func Tokenize(source []byte) []Token {
-	lex := NewLexer(source)
-
-	for !lex.atEOF() {
-		matched := false
-
-		for _, pattern := range lex.patterns {
-			loc := pattern.regex.FindStringIndex(string(lex.remainder()))
-
-			if loc != nil && loc[0] == 0 {
-				pattern.handler(lex, pattern.regex)
-				matched = true
-				break
-			}
-		}
-
-		if !matched {
-			panic(fmt.Sprintf("Lexer::Error -> unrecoginized token at line %d column %d near %s\n", lex.line, lex.column, string(lex.remainder())))
-		}
-	}
-
-	lex.push(NewToken(EOF, "EOF", lex.line, lex.column))
-
-	return lex.Tokens
-}
-
-func numberHandler(lex *Lexer, regex *regexp.Regexp) {
-	match := regex.FindString(string(lex.remainder()))
-	lex.push(NewToken(NUMBER, match, lex.line, lex.column))
-	lex.advanceN(len(match))
-}
-func skipHandler(lex *Lexer, regex *regexp.Regexp) {
-	match := regex.FindStringIndex(string(lex.remainder()))
-	lex.advanceN(match[1])
-}
-func defaultHandler(kind TokenKind, value string) regexHandler {
-	return func(lex *Lexer, regex *regexp.Regexp) {
-		lex.advanceN(len(value))
-		lex.push(NewToken(kind, value, lex.line, lex.column))
 	}
 }
