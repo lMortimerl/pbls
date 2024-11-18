ce4ac78747051af379c59e3533070e8f432883f1
Moritmer Gibbons <mkirchermeier@gmail.com>
2024-11-18 20:48:22 +0100

    Type parsing Added funtionality to parse types. Eg:     string ls_str = "ABC"     int li_int = 0     const string lcs_str = "ACB"

--- src/parser/stmt.go
+++ src/parser/stmt.go
@@ -19,33 +19,55 @@ func parse_stmt(p *parser) ast.Stmt {
 }
 
 func parse_var_decl_stmt(p *parser) ast.Stmt {
-	var explicitType ast.Type
-	var assignedValue ast.Expr
-	isConstant := p.advance().Kind == lexer.CONSTANT
-	varName := p.expectError(lexer.IDENTIFIER, "Expected varname after constant").Value
-
-	if p.currentToken().Kind == lexer.COLON {
+	var isConstant bool = false
+	var varValue ast.Expr
+	var possibleType ast.Type
+	if p.currentToken().Kind == lexer.CONSTANT {
 		p.advance()
-		explicitType = parse_type(p, default_bp)
+		isConstant = true
 	}
-
-	if p.currentToken().Kind != lexer.SEMICOLON {
-		p.expectError(lexer.EQUALS, "Expected = after varname")
-		assignedValue = parse_expr(p, assignment)
-	} else if explicitType == nil {
-		panic("Missing either righthand side in var declaration or explicit type.")
+	possibleType = parse_type(p, default_bp)
+	varName := p.advance()
+	if p.advance().Kind == lexer.EQUALS {
+		varValue = parse_expr(p, default_bp)
 	}
-
-	p.expectError(lexer.SEMICOLON, "Expected Semicolon!")
-
-	if isConstant && assignedValue == nil {
-		panic("Cannot define Constant without providing value")
-	}
+	p.expectError(lexer.SEMICOLON, "Expected Semicolon")
 
 	return ast.VarDeclStmt{
-		Identifier:    varName,
+		Identifier:    varName.Value,
 		IsConstant:    isConstant,
-		AssignedValue: assignedValue,
-		ExplicitType:  explicitType,
+		AssignedValue: varValue,
+		ExplicitType:  possibleType,
 	}
+	/*
+	   var explicitType ast.Type
+	   var assignedValue ast.Expr
+	   isConstant := p.advance().Kind == lexer.CONSTANT
+	   varName := p.expectError(lexer.IDENTIFIER, "Expected varname after constant").Value
+
+	   if p.currentToken().Kind == lexer.COLON {
+	       p.advance()
+	       explicitType = parse_type(p, default_bp)
+	   }
+
+	   if p.currentToken().Kind != lexer.SEMICOLON {
+	       p.expectError(lexer.EQUALS, "Expected = after varname")
+	       assignedValue = parse_expr(p, assignment)
+	   } else if explicitType == nil {
+	       panic("Missing either righthand side in var declaration or explicit type.")
+	   }
+
+	   p.expectError(lexer.SEMICOLON, "Expected Semicolon!")
+
+	   if isConstant && assignedValue == nil {
+	       panic("Cannot define Constant without providing value")
+	   }
+
+	   return ast.VarDeclStmt{
+	       Identifier:    varName,
+	       IsConstant:    isConstant,
+	       AssignedValue: assignedValue,
+	       ExplicitType:  explicitType,
+	   }
+	*/
 }
